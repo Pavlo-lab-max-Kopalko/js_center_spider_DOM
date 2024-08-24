cd6ba0e6ac5d1e8a2954ace6ac6434fd81185135
Pavlo <oksanakopalko@gmail.com>
2024-08-24 19:36:48 +0300

    make build

---
+++ index.b848b177.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".spider"),t=document.querySelector(".wall"),c=t.clientHeight,l=t.clientWidth,i=e.clientHeight,n=e.clientWidth;e.style.top="".concat((c-i)/2,"px"),e.style.left="".concat((l-n)/2,"px");
+//# sourceMappingURL=index.b848b177.js.map
